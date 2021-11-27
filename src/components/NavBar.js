7d7d25d1ba8b460f9ab61981186c6a6f79173e33
Mit Amin <mit.amin@hinge.co>
2021-11-26 21:28:10 -0500

    updated styling of navbar and added social media links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,38 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() { 
     return ( 
-        <header>
-            <div>
+        <header className="bg-red-600">
+            <div className="container mx-auto flex justify-between">
                 <nav>
-                    <NavLink to="/" exact>
+                    <NavLink to="/" 
+                        exact 
+                        activeClassName="text-white"
+                        className = "inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold tracking-widest"
+                    >
                         Mit
                     </NavLink>
-                    <NavLink to="/post">
+                    <NavLink 
+                        to="/post" 
+                        className = "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                        activeClassName="text-red-100 bg-red-700"
+                    >
                         Blog Posts
                     </NavLink>
-                    <NavLink to="/project"> 
+                    <NavLink to="/project" className = "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"> 
                         Projects
                     </NavLink>
-                    <NavLink to="/about">
+                    <NavLink to="/about" className = "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
                         About Me!
                     </NavLink>
                 </nav>
+                <div className="inline-flex py-3 px-3 my-6"> 
+                    <SocialIcon url="https://www.linkedin.com/in/mitaminvcu/" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} /> 
+                    <SocialIcon url="https://twitter.com/FancyMitten" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} /> 
+                    <SocialIcon url="https://www.facebook.com/mit.amin.1" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} /> 
+                </div>
             </div>
         </header>
     )
